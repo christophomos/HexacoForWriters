0a03754e3886e77fb9482621886313da94b17714
Chris Clark <chris@qalang.com>
2020-08-02 08:50:05 +0800

    Initial Commit

---
+++ Shared/HexacoForWritersApp.swift
@@ -0,0 +1,17 @@
+//
+//  HexacoForWritersApp.swift
+//  Shared
+//
+//  Created by Chris Clark on 2020/8/2.
+//
+
+import SwiftUI
+
+@main
+struct HexacoForWritersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
